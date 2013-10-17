ab8e01a00876f997f98b8cdd5042f6268710a6d8
sarah <sarah@triggersandsparks.com>
2013-10-17 13:42:17 -0400

    Set up basic styling of page panels.

--- page-onepage.php
+++ page-onepage.php
@@ -6,22 +6,23 @@ Template Name: One-page site
 
 <?php get_header(); ?>
 
-			<div id="content">
 
-				<div id="inner-content" class="wrap clearfix">
-
-						<div id="main" class="twelvecol first last clearfix" role="main">
 
 								<?php
 								
 								// Let's show all the static pages! Whoo-hoo!
 								$pages = get_pages(array('child_of' => $post->ID, 'sort_column' => 'menu_order', 'sort_order' => 'asc')); 
 								foreach ($pages as $page_data) {
-									echo '<div class="twelvecol first last clearfix" style="background: purple; margin-top: 2em;">';
 									$content = apply_filters('the_content', $page_data->post_content); 
 									$title = $page_data->post_title; 
-									echo "<h2>" . $title . "</h2>";
-									echo $content; 
+									$slug = $page_data->post_name;
+									echo '<div id="'.$slug.'" class="page-panel">';
+										echo '<div class="inner-content wrap clearfix">';
+											echo '<div class="twelvecol first last clearfix entry-content" role="main">';
+												echo "<h2>" . $title . "</h2>";
+												echo $content; 
+											echo '</div>';
+										echo '</div>';
 									echo '</div>';
 								}
 								?>
