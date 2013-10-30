18299f90935b5093c46ba9d78eaf4fdb3ba14d87
sarah <sarah@triggersandsparks.com>
2013-10-30 17:34:27 -0400

    Conditional logic in one-page display should rely on page slugs, not titles, so that titles can be changed as desired.

--- page-onepage.php
+++ page-onepage.php
@@ -21,17 +21,17 @@ Template Name: One-page site
 									<div id="<?php echo $slug; ?>" class="page-panel" data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
 										<div class="inner-content wrap clearfix">
 											<div class="twelvecol first last clearfix entry-content" role="main">
-												<?php if ($title != "Home"):  echo '<h1>' . $title . '</h1>';  endif; ?>
+												<?php if ($slug != "home"):  echo '<h1>' . $title . '</h1>';  endif; ?>
 												<?php echo $content; ?>
 												
-												<?php if ($title == "News"): ?>
+												<?php if ($slug == "news"): ?>
 													<?php get_template_part('news'); ?>
 												<?php endif; ?>	
 											
 											</div>
 										</div>
 										
-										<?php if ($title == "Home"): ?>
+										<?php if ($slug == "home"): ?>
 										
 										<?php endif; ?>
 										
