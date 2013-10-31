5d5232a91f8179595702b8c6b370e347100ae3c6
sarah <sarah@triggersandsparks.com>
2013-10-30 23:34:11 -0400

    Remove stupid torn-page effect and replace with simpler between-pages panel. Needs a better background and some content.

--- page-onepage.php
+++ page-onepage.php
@@ -6,48 +6,36 @@ Template Name: One-page site
 
 <?php get_header(); ?>
 
+<?php // Let's show all the static pages!
+$pages = get_pages(array('child_of' => $post->ID, 'sort_column' => 'menu_order', 'sort_order' => 'asc')); 
+$count = 1;
+foreach ($pages as $page_data) :
+	$content = apply_filters('the_content', $page_data->post_content); 
+	$title = $page_data->post_title; 
+	$slug = $page_data->post_name;
+	?>
+	<div id="<?php echo $slug; ?>" class="page-panel" data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
+		<div class="inner-content wrap clearfix">
+			<div class="twelvecol first last clearfix entry-content" role="main">
+				<?php if ($slug != "home"):  echo '<h1>' . $title . '</h1>';  endif; ?>
+				<?php echo $content; ?>
+				
+				<?php if ($slug == "news"): ?>
+					<?php get_template_part('news'); ?>
+				<?php endif; ?>	
+			
+			</div>
+		</div>
+		
+		<a class="down-button" data-slide="<?php echo ++$count; ?>" title=""></a>
+	</div>
+	
+<div class="middle-slide" data-stellar-background-ratio="4.5">
+	<div>Put patterns, images, etc here to break up the pages.</div>
+</div>
+	
+<?php endforeach; ?>
 
-
-								<?php
-								
-								// Let's show all the static pages! Whoo-hoo!
-								$pages = get_pages(array('child_of' => $post->ID, 'sort_column' => 'menu_order', 'sort_order' => 'asc')); 
-								$count = 1;
-								foreach ($pages as $page_data) :
-									$content = apply_filters('the_content', $page_data->post_content); 
-									$title = $page_data->post_title; 
-									$slug = $page_data->post_name;
-									?>
-									<div id="<?php echo $slug; ?>" class="page-panel" data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
-										<div class="inner-content wrap clearfix">
-											<div class="twelvecol first last clearfix entry-content" role="main">
-												<?php if ($slug != "home"):  echo '<h1>' . $title . '</h1>';  endif; ?>
-												<?php echo $content; ?>
-												
-												<?php if ($slug == "news"): ?>
-													<?php get_template_part('news'); ?>
-												<?php endif; ?>	
-											
-											</div>
-										</div>
-										
-										<?php if ($slug == "home"): ?>
-											<div id="paper-edge-top"></div>
-										<?php endif; ?>
-										
-										<?php if ($slug == "books"): ?>
-											<div id="paper-edge-bottom"></div>
-										<?php endif; ?>
-										
-										<a class="down-button" data-slide="<?php echo ++$count; ?>" title=""></a>
-									</div>
-									
-								<?php if ($slug == "home"): ?>
-											<div id="paper-edge-middle">Want some in-the-middle text?</div>
-										<?php endif; ?>
-									
-								<?php endforeach; ?>
-							
-							<img id="absinthe" src="<?php echo get_template_directory_uri(); ?>/library/images/smoke.png" alt="smoke" width="547" height="820" data-stellar-ratio="3.5"/>
+<img id="absinthe" src="<?php echo get_template_directory_uri(); ?>/library/images/smoke.png" alt="smoke" width="547" height="820" data-stellar-ratio="3.5"/>
 
 <?php get_footer(); ?>
