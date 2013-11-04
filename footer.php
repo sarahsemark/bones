abcc50c15c651193df5882a25e32f59a1b1178d8
sarah <sarah@triggersandsparks.com>
2013-11-04 13:09:50 -0500

    Add Gary's illustration credit to footer ' '

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 							<?php // bones_footer_links(); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Website designed &amp; developed by <a href="http://triggersandsparks.com">Triggers &amp; Sparks</a>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Website designed &amp; developed by <a href="http://triggersandsparks.com">Triggers &amp; Sparks</a>. Illustration by <a href="http://www.hannastudios.com">Hanna Studios</a>.</p>
 
 				</div> <!-- end #inner-footer -->
 
